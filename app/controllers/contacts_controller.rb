ed9a0cf3592d34723d7789b7b588ae0f8854677f
Kievbuy <kievbuy@gmail.com>
2017-08-22 21:07:47 +0300

    Save Contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,16 @@
 class ContactsController < ApplicationController
   
   def new
-  
   end
   
   def create
-  
+    @contact = Contact.new(content_params)
+    @contact.save
   end
-  
+
+  private
+
+  def content_params
+    params.require(:contact).permit(:name, :phone, :email, :message)
+  end
 end
