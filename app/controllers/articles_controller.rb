256605646de84bada8288eb738d3c8dcc98e057c
Kievbuy <kievbuy@gmail.com>
2017-08-25 17:08:29 +0300

    Add destoy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,15 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to @article
+    else
+      render action: 'index'
+    end
+  end
+  
   private
 
   def article_params
